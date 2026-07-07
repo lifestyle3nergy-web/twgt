2736a38997a54c031b8ee1f6c84ab3aa2f56fc64
TradieNate <260296968+lifestyle3nergy-web@users.noreply.github.com>
2026-07-08 11:53:40 +1200

    feat(core): implement service provider

--- src/core/ServiceProvider.ts
+++ src/core/ServiceProvider.ts
@@ -1,1 +1,19 @@
+import { Container } from "./Container";
 
+export class ServiceProvider {
+  constructor(private readonly container: Container) {}
+
+  /**
+   * Resolve a registered service.
+   */
+  public get<T>(key: string): T {
+    return this.container.resolve<T>(key);
+  }
+
+  /**
+   * Check whether a service is registered.
+   */
+  public has(key: string): boolean {
+    return this.container.has(key);
+  }
+}
