058e39513aeca384a8a8e2cf28d186a557540d42
TradieNate <260296968+lifestyle3nergy-web@users.noreply.github.com>
2026-07-07 20:44:27 +1200

    feat: add core application lifecycle

---
+++ src/core/Application.ts
@@ -0,0 +1,13 @@
+export class Application {
+  public async initialize(): Promise<void> {
+    console.log("Initializing TWGT platform...");
+  }
+
+  public async start(): Promise<void> {
+    console.log("Starting TWGT platform...");
+  }
+
+  public async stop(): Promise<void> {
+    console.log("Stopping TWGT platform...");
+  }
+}
